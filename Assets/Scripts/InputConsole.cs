01a2576ba8fc21499017b056d7a83b726484fd16
mattstg <mattstger@gmail.com>
2017-06-09 08:27:14 -0400

    input console
    started adding that, the game has compiler error atm, mid push

---
+++ Assets/Scripts/InputConsole.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputConsole : MonoBehaviour {
+
+    public UnityEngine.UI.InputField inputFeild;
+
+    public void InputEntered()
+    {
+        string currentText = inputFeild.text;
+        //do stuff
+        Debug.Log("text: " + currentText);
+        inputFeild.text = "";
+    }
+}
